6f0cbc88c27c731f1e1e0b8536f23f253a1d83ad
jaswanth_270602 <pjaswanthkumar270602@gmail.com>
2025-02-24 08:58:45 +0530

    feed

--- src/app.js
+++ src/app.js
@@ -8,7 +8,27 @@ const app = express();
 const PORT = 3000;
 app.use(express.json())
 
+app.get('/user', async (req, res) => {
+    const email = req.body.email;
+    try {
+        const user = await User.findOne({ emailId : email});
+        if(user.length === 0) {
+            res.status(404).send("User not found");
+        }
+        res.status(200).send(user);
+    } catch( err ) {
+        res.status(400).send("something went wrong");
+    } 
+})
 
+app.get("/feed", async(req, res) => {
+    try {
+        const users = await User.find();
+        res.send(users);
+    } catch (err) {
+        res.status(400).send("something went wrong");
+    }
+})
 app.post('/signup', async (req, res) => {
 
     const user = new User(req.body);
